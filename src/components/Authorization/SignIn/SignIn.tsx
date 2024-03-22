eb36579870ca792fed5b6f8845db4e757567f30c
Filipp <feelising@gmail.com>
2024-03-22 23:12:45 +0600

    auth with firebase

--- src/components/Authorization/SignIn/SignIn.tsx
+++ src/components/Authorization/SignIn/SignIn.tsx
@@ -1,12 +1,24 @@
 "use client";
-import { Button, Checkbox, Flex, Input, Space } from "antd";
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Input,
+  Space,
+  Typography,
+  notification,
+} from "antd";
 import React, { useState } from "react";
 import {
   useAuthState,
   useSignInWithEmailAndPassword,
+  useSignInWithEmailLink,
 } from "react-firebase-hooks/auth";
 import { auth } from "../../../../configs/firebase/config";
 import style from "./style.module.scss";
+import { NotificationPlacement } from "antd/es/notification/interface";
+import { BorderTopOutlined } from "@ant-design/icons";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 interface SignInProps {
   setActiveModal: Function;
@@ -16,53 +28,96 @@ const SignIn: React.FC<SignInProps> = ({ setActiveModal }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
-  //   const [error, setError] = useState("");
-  //   console.log(error);
-  const [user, loading] = useAuthState(auth);
-  const [SignInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
-  const handleSignIn = async () => {
-    await SignInWithEmailAndPassword(email, password)
-      .then((res) => console.log(res))
-      .catch((err) => console.error({ err }));
+  const [error, setError] = useState(false);
+  // const [user, loading] = useAuthState(auth);
+  const auth = getAuth();
+  const [api, contextHolder] = notification.useNotification();
 
-    if (user) {
-      console.log("object");
-      setActiveModal(false);
+  const openNotification = (placement: NotificationPlacement) => {
+    api.error({
+      message: `Error`,
+      description: "Invalid Email or password, please, try again!",
+      placement,
+    });
+  };
+  const fetchData = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      return res;
+    } catch (error) {
+      console.log(typeof error);
+      throw error;
     }
+  };
+  // console.log(email, password, user);
+  // const [SignInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const res = await SignInWithEmailAndPassword(email, password);
+    // if (!res) {
+    //   openNotification("top");
+    //   setError(true);
+    // }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+      });
   };
 
   return (
-    <Space direction="vertical">
-      <Input
-        value={email}
-        placeholder="Basic usage"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Flex gap={10}>
-        <Input.Password
-          value={password}
-          className={style.input_pass}
-          placeholder="input password"
-          visibilityToggle={{
-            visible: passwordVisible,
-            onVisibleChange: setPasswordVisible,
-          }}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <>
+      {contextHolder}
 
-        <Button
-          type="default"
-          style={{ width: 80 }}
-          onClick={() => setPasswordVisible((prevState) => !prevState)}
-        >
-          {passwordVisible ? "Hide" : "Show"}
-        </Button>
-      </Flex>
-      <Checkbox onChange={() => console.log()}>Remember me</Checkbox>
-      <Button type="primary" onClick={handleSignIn}>
-        Sign in
-      </Button>
-    </Space>
+      <form onSubmit={(e) => handleSignIn(e)}>
+        <Space direction="vertical">
+          <Input
+            status={error ? "error" : ""}
+            value={email}
+            placeholder="Basic usage"
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setError(false);
+            }}
+          />
+          <Flex gap={10}>
+            <Input.Password
+              status={error ? "error" : ""}
+              value={password}
+              className={style.input_pass}
+              placeholder="input password"
+              visibilityToggle={{
+                visible: passwordVisible,
+                onVisibleChange: setPasswordVisible,
+              }}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError(false);
+              }}
+            />
+
+            <Button
+              type="default"
+              style={{ width: 80 }}
+              onClick={() => setPasswordVisible((prevState) => !prevState)}
+            >
+              {passwordVisible ? "Hide" : "Show"}
+            </Button>
+          </Flex>
+          <Checkbox onChange={() => console.log()}>Remember me</Checkbox>
+          <Button type="primary" htmlType="submit">
+            Sign in
+          </Button>
+        </Space>
+      </form>
+    </>
   );
 };
 export default SignIn;
--- src/components/Authorization/SignUp/SignUp.tsx
+++ src/components/Authorization/SignUp/SignUp.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { Button, Checkbox, Flex, Input, Space } from "antd";
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Input,
+  Space,
+  Tooltip,
+  notification,
+} from "antd";
 import style from "./style.module.scss";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../../../configs/firebase/config";
+import { NotificationPlacement } from "antd/es/notification/interface";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 interface SignUpProps {}
 
@@ -12,67 +22,121 @@ const SignUp: React.FC<SignUpProps> = ({}) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [api, contextHolder] = notification.useNotification();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const auth = getAuth();
+  const validRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const [createUserWithEmailAndPassword] =
-    useCreateUserWithEmailAndPassword(auth);
-  const handleSignUp = async () => {
-    createUserWithEmailAndPassword(email, password)
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error.message));
+  const openNotification = (placement: NotificationPlacement) => {
+    api.error({
+      message: `Error`,
+      description: "Email is exict!",
+      placement,
+    });
+  };
+
+  // const [createUserWithEmailAndPassword] =
+  //   useCreateUserWithEmailAndPassword(auth);
+
+  const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        setErrorMessage(error.message);
+        openNotification("top");
+      });
   };
 
   return (
-    <Space direction="vertical">
-      <Input
-        defaultValue={email}
-        placeholder="Basic usage"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Flex gap={10}>
-        <Input.Password
-          defaultValue={password}
-          className={style.input_pass}
-          placeholder="input password"
-          visibilityToggle={{
-            visible: passwordVisible,
-            onVisibleChange: setPasswordVisible,
-          }}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <>
+      {contextHolder}
+      <form onSubmit={(e) => handleSignUp(e)}>
+        <Space direction="vertical">
+          <Tooltip
+            trigger={["focus"]}
+            title={emailError ? "Invalid email" : undefined}
+            placement="topLeft"
+            overlayClassName="numeric-input"
+          >
+            <Input
+              status={emailError ? "error" : ""}
+              defaultValue={email}
+              placeholder="Basic usage"
+              onChange={(e) => {
+                setEmail(e.target.value);
+                if (!e.target.value.toLowerCase().match(validRegex)) {
+                  setEmailError(true);
+                }
+                if (e.target.value.toLowerCase().match(validRegex)) {
+                  setEmailError(false);
+                }
+              }}
+            />
+          </Tooltip>
+          <Flex gap={10}>
+            <Input.Password
+              defaultValue={password}
+              className={style.input_pass}
+              placeholder="input password"
+              visibilityToggle={{
+                visible: passwordVisible,
+                onVisibleChange: setPasswordVisible,
+              }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-        <Button
-          type="default"
-          style={{ width: 80 }}
-          onClick={() => setPasswordVisible((prevState) => !prevState)}
-        >
-          {passwordVisible ? "Hide" : "Show"}
-        </Button>
-      </Flex>
-      <Flex gap={10}>
-        <Input.Password
-          defaultValue={confirmPassword}
-          className={style.input_pass}
-          placeholder="Confirm password"
-          visibilityToggle={{
-            visible: passwordVisible,
-            onVisibleChange: setPasswordVisible,
-          }}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
+            <Button
+              type="default"
+              style={{ width: 80 }}
+              onClick={() => setPasswordVisible((prevState) => !prevState)}
+            >
+              {passwordVisible ? "Hide" : "Show"}
+            </Button>
+          </Flex>
+          <Flex gap={10}>
+            <Input.Password
+              status={confirmPassword === password ? "" : "error"}
+              defaultValue={confirmPassword}
+              className={style.input_pass}
+              placeholder="Confirm password"
+              visibilityToggle={{
+                visible: confirmPasswordVisible,
+                onVisibleChange: setConfirmPasswordVisible,
+              }}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
 
-        <Button
-          type="default"
-          style={{ width: 80 }}
-          onClick={() => setPasswordVisible((prevState) => !prevState)}
-        >
-          {passwordVisible ? "Hide" : "Show"}
-        </Button>
-      </Flex>
-      <Checkbox onChange={() => console.log()}>Remember me</Checkbox>
-      <Button type="primary" onClick={handleSignUp}>
-        Sign in
-      </Button>
-    </Space>
+            <Button
+              type="default"
+              style={{ width: 80 }}
+              onClick={() =>
+                setConfirmPasswordVisible((prevState) => !prevState)
+              }
+            >
+              {confirmPasswordVisible ? "Hide" : "Show"}
+            </Button>
+          </Flex>
+          <Checkbox onChange={() => console.log()}>Remember me</Checkbox>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={!(confirmPassword === password && !emailError)}
+          >
+            Sign in
+          </Button>
+        </Space>
+      </form>
+    </>
   );
 };
 export default SignUp;

e2d92ed784d1bd65a1c659a5ab8af6f4a75ab66b
Чертков Филипп Олегович <fchertkov@phoenixit.ru>
2024-09-04 20:20:21 +0300

    ru lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='ru'>
       <body className=''>
         <Providers>
           <MainLayout>

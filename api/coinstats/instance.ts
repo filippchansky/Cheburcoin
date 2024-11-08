f2e4d70cd39bf9042b24349e6ac550d9c9253a88
filippchansky <filippchansky@yandex.ru>
2024-11-08 18:20:23 +0300

    rising from the dead

--- api/coinstats/instance.ts
+++ api/coinstats/instance.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_COIN_API
+  baseURL: process.env.NEXT_PUBLIC_COIN_API,
+  headers: {
+    accept: 'application/json',
+    'X-API-KEY': 'qXg0vsCcTpgTJieqIrzBkXN1hUvFhT3tQ9n155+PO5o='
+  }
 });

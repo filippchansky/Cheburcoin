16ab5eaf4101d6396652284e020f474d3364bfcb
filippchansky <filippchansky@yandex.ru>
2025-01-31 14:45:58 +0300

    hidden api key

--- api/coinstats/instance.ts
+++ api/coinstats/instance.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_COIN_API,
   headers: {
     accept: 'application/json',
-    'X-API-KEY': 'qXg0vsCcTpgTJieqIrzBkXN1hUvFhT3tQ9n155+PO5o='
+    'X-API-KEY': process.env.NEXT_PUBLIC_COIN
   }
 });
